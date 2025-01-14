1672e30ff0a4951b13ada126063ff9b42e6c8d09
Henry Lovaday <henrygad.orji@gmail.com>
2025-01-14 16:56:39 +0100

    tenth commit (remove dist folder)

--- dist/utils/hideEmail.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hideEmail = (email) => {
-    // Function to hide user email
-    const [username, domain] = email.split("@");
-    return `${username.slice(0, 3)}***@${domain}`;
-};
-exports.default = hideEmail;
