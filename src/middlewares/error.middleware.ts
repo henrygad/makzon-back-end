1f7ee2e2059979e4591e9dbb900e23b891653117
Henry Loveday <henrygad.orji@gmail.com>
2025-07-09 12:03:20 +0100

    adjuctment made on error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -4,17 +4,24 @@ interface ErrorWithStatus extends Error {
   statusCode?: number;
   status?: string;
   isOperational?: boolean;
+  response: {
+    data: string
+  }
 }
 // Global error middleware
 const errorHandler = (err: ErrorWithStatus, req: Request, res: Response, next: NextFunction) => {
   err.statusCode = err.statusCode || 500;
-  res.status(err.statusCode).json({
-    ...err,
-    message: err.message || "Server Error",
-    stack: err.stack, // process.env.NODE_ENV === "production" ? null : err.stack,
-    date: Date(),
-    targetUrl: req.originalUrl
-  });
+
+  res.
+    status(err.statusCode)
+    .json({
+      ...(err.response?.data && { details: err.response.data }),
+      message: err.message || "Server Error",
+      stack: err.stack, // process.env.NODE_ENV === "production" ? null : err.stack,
+      date: Date(),
+      targetUrl: req.originalUrl,
+      name: err.name,
+    });
 
   next();
 };
